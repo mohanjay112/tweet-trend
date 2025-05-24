7e49167c1d5c6ce0ccdea26042a5dd4e22c41c27
mohanjay112 <mohanjay112@gmail.com>
2025-05-24 20:38:50 +0530

    updatedd versio

--- src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/valaxy/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Greetings from Tech Jay Tech";
+		return "Greetings from Tech J@y Tech";
 	}
 
 	@GetMapping("/trends")
